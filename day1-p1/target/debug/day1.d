518ec4456eed45acac8a01a1bbee0fd41bd02be4
Josh Abraham <sinisterpatrician@gmail.com>
2018-12-02 18:25:28 -0500

    Gitignore now enforcing

--- day1-p1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/josh/Projects/advent_of_code/day1/target/debug/day1: /home/josh/Projects/advent_of_code/day1/src/main.rs
--- day1-p2/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/josh/Projects/advent_of_code/day1-p2/target/debug/day1: /home/josh/Projects/advent_of_code/day1-p2/src/main.rs
